60e2e702158ba5ba46837da6c9b35fc5bf3f82a6
Luke Edgecombe <145694364+Luk446@users.noreply.github.com>
2024-02-27 23:45:49 +0000

    Add project files.

---
+++ SerialPortExample/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace SerialPortExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // serialPort
+            // 
+            this.serialPort.PortName = "COM3";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ SerialPortExample/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace SerialPortExample
+{
+    public partial class Form1 : Form
+    {
+        // Declare the class-level serialPort variable here
+        //private SerialPort serialPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeSerialPort();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitializeSerialPort()
+        {
+            // Initialize the SerialPort object within this method
+            SerialPort port = new SerialPort();
+            port.PortName = "COM3";
+            port.BaudRate = 9600;
+            port.DataReceived += SerialPort_DataReceived;
+            port.Open();
+
+            // Assign the initialized SerialPort object to the class-level variable
+            serialPort = port;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = serialPort.ReadLine();
+            UpdateUI(data);
+        }
+
+        private void UpdateUI(string data)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<string>(UpdateUI), data);
+            }
+            else
+            {
+                textBox1.AppendText(data + Environment.NewLine);
+            }
+        }
+    }
+}
